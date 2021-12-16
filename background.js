eedf360a5d95d0f0c70fb5b3df562e5480331dbe
jsvirtane <virtanenjami@gmail.com>
2021-12-16 16:47:38 +0200

    first commit, and basics

---
+++ background.js
@@ -0,0 +1,58 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "search_all",
+    title: 'Etsi kaikista luetteloista: "%s"',
+    contexts: ["selection"],
+  })
+})
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "MP_search",
+    title: 'Etsi Moto-Profilin luettelosta: "%s"',
+    contexts: ["selection"],
+  })
+})
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "Kaha_search",
+    title: 'Etsi Autoluettelosta luettelosta: "%s"',
+    contexts: ["selection"],
+  })
+})
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "MP_VIN_search",
+    title: 'VIN-haku Moto-Profil: "%s"',
+    contexts: ["selection"],
+  })
+})
+
+chrome.contextMenus.onClicked.addListener(function (search, tab) {
+  switch (search.menuItemId) {
+    case "MP_search":
+      chrome.tabs.create({
+        url: `https://katalog.profiauto.net/artykuly/szukaj?s=${search.selectionText}`,
+      })
+      break
+    case "Kaha_search":
+      chrome.tabs.create({
+        url: `https://www.autoluettelo.fi/wls/regno_search.do?country=FI&regNo=&freetext=${search.selectionText}`,
+      })
+      break
+    case "MP_VIN_search":
+      chrome.tabs.create({
+        url: `https://katalog.profiauto.net/vin/SzukajVin?s=${search.selectionText}`,
+      })
+      break
+    default:
+      chrome.tabs.create({
+        url: `https://katalog.profiauto.net/artykuly/szukaj?s=${search.selectionText}`,
+      })
+      chrome.tabs.create({
+        url: `https://www.autoluettelo.fi/wls/regno_search.do?country=FI&regNo=&freetext=${search.selectionText}`,
+      })
+  }
+})
