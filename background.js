fa36863339a2242732968e37f6217182ec995690
jsvirtane <virtanenjami@gmail.com>
2021-12-16 17:35:50 +0200

    added new tabs to open right from current tab

--- background.js
+++ background.js
@@ -35,24 +35,29 @@ chrome.contextMenus.onClicked.addListener(function (search, tab) {
     case "MP_search":
       chrome.tabs.create({
         url: `https://katalog.profiauto.net/artykuly/szukaj?s=${search.selectionText}`,
+        index: tab.index + 1
       })
       break
     case "Kaha_search":
       chrome.tabs.create({
         url: `https://www.autoluettelo.fi/wls/regno_search.do?country=FI&regNo=&freetext=${search.selectionText}`,
+        index: tab.index + 1,
       })
       break
     case "MP_VIN_search":
       chrome.tabs.create({
         url: `https://katalog.profiauto.net/vin/SzukajVin?s=${search.selectionText}`,
+        index: tab.index + 1,
       })
       break
     default:
       chrome.tabs.create({
         url: `https://katalog.profiauto.net/artykuly/szukaj?s=${search.selectionText}`,
+        index: tab.index + 1,
       })
       chrome.tabs.create({
         url: `https://www.autoluettelo.fi/wls/regno_search.do?country=FI&regNo=&freetext=${search.selectionText}`,
+        index: tab.index + 1,
       })
   }
 })
